7559b9ef5de82ca3d10b93ac322f0b8f3c639c5e
Ajay Maurya <C01637@launcher.int.abnamro.com>
2020-11-16 13:13:01 +0100

    Initial Commit for Read Image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { createWorker } from 'tesseract.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'readImage';
+  
+  fileChange(files: FileList){
+    let file: File = files[0];
+    this.doOCR(file).then(() =>{
+      console.log("Completed");
+    },()=>{
+      console.log("Failed");
+    });
+  }
+
+  async doOCR(file) {
+    const worker = createWorker({
+      logger: m => console.log(m),
+    });
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const {data : { text }} = await worker.recognize(file);
+    console.log(text);
+    await worker.terminate();
+  }
 }
